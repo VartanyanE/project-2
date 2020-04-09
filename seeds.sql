2803d186e1970bbe9784b0fc51c64373a21edb33
mikevampm323 <mike.vartanyan@yahoo.com>
2020-04-08 20:27:52 -0700

    user_name

--- seeds.sql
+++ seeds.sql
@@ -1,15 +1,20 @@
 USE inventory_db;
 
-INSERT INTO Users (email,password,createdAt,updatedAt) VALUES('myself@gmail.com','password','2020-04-08 04:43:40','2020-04-08 04:43:40');
+INSERT INTO Users
+    (email,password,createdAt,updatedAt, user_name)
+VALUES('myself@gmail.com', 'password', '2020-04-08 04:43:40', '2020-04-08 04:43:40', 'Martin Funches');
 
 
-INSERT INTO inventory (item_name,item_format,number_items,user_id,createdAt,updatedAt)
-VALUES('Hormel Chili','can','10',1,'2020-04-08 04:43:40','2020-04-08 04:43:40');   
+INSERT INTO inventory
+    (item_name,item_format,number_items,user_id,createdAt,updatedAt)
+VALUES('Hormel Chili', 'can', '10', 1, '2020-04-08 04:43:40', '2020-04-08 04:43:40');
 
-INSERT INTO inventory (item_name,item_format,number_items,user_id,createdAt,updatedAt)
-VALUES('Cup of Noodles','cup','20',1,'2020-04-08 04:43:40','2020-04-08 04:43:40'); 
+INSERT INTO inventory
+    (item_name,item_format,number_items,user_id,createdAt,updatedAt)
+VALUES('Cup of Noodles', 'cup', '20', 1, '2020-04-08 04:43:40', '2020-04-08 04:43:40');
 
-INSERT INTO inventory (item_name,item_format,number_items,user_id,createdAt,updatedAt)
-VALUES('Chicken of the Sea','can','30',1,'2020-04-08 04:43:40','2020-04-08 04:43:40')     
+INSERT INTO inventory
+    (item_name,item_format,number_items,user_id,createdAt,updatedAt)
+VALUES('Chicken of the Sea', 'can', '30', 1, '2020-04-08 04:43:40', '2020-04-08 04:43:40')     
  
     
