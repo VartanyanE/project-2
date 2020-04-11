8cf54390f09712a367f4cc9c8d6962be89c76c68
Stephanie DeDios <stephtdedios@gmail.com>
2020-04-11 13:49:34 -0700

    updated News API to NY Times

--- public/js/covid.js
+++ public/js/covid.js
@@ -1,10 +1,22 @@
+var jumboHeight = $(".jumbotron").outerHeight();
+function parallax() {
+  var scrolled = $(window).scrollTop();
+  $(".bg").css("height", jumboHeight - scrolled + "px");
+}
+
+$(window).scroll(function (e) {
+  parallax();
+});
+
 var api_url = "https://covidtracking.com/api/v1/states/current.json";
+var apiDaily =
+  "https://covidtracking.com/api/v1/states/daily?state=NY&date=20200316";
+var state = "";
 
 fetch(api_url).then((res) => {
   res.json().then((data) => {
     console.log(data);
-    if ((data.length = 1));
-    var state = "";
+    if ((data.length = 50));
 
     data.forEach((u) => {
       state += "<tr>";
@@ -12,31 +24,42 @@ fetch(api_url).then((res) => {
       state += "<td>" + u.negative + "</td>";
       state += "<td>" + u.positive + "</td>";
       state += "<td>" + u.death + "</td>";
-      state += "<td>" + u.dateChecked + "</td></tr>";
     });
     document.getElementById("data").innerHTML = state;
   });
 });
 
-function setup() {
-  var button = select("#button-addon2");
-  button.mousePressed(stateDesired);
-}
+$("#button-addon2").on("click", function () {
+  var search = $("#search").val().trim();
+  var stateUrl = `https://covidtracking.com/api/states?state=${search}`;
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: stateUrl,
+    method: "GET",
+  };
+  $("#search").val("");
 
-function stateDesired() {
-  var url = api_url + state;
-  loadJSON(url, gotData);
-}
-
-function gotData() {
-  stateDesired = data;
-}
-
-var apiNews = "https://covidtracking.com/api/press";
-
-fetch(apiNews).then((res) => {
-  res.json().then((data) => {
-    console.log(data);
-    if (data.length < data.length);
+  $.ajax(settings).done((data) => {
+    document.getElementById("data").innerHTML = state;
   });
 });
+
+var url =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=coronavirus&api-key=YMc1gccWtljsNRIaEKZEZBHnRni3CLGx";
+
+$.ajax({
+  url: url,
+  method: "GET",
+})
+  .done(function (result) {
+    console.log(result);
+    var i;
+    for (i = 0; i < 5; i++) {
+      document.getElementById("articleTitle").innerHTML =
+        result.response.docs[i].headline.main;
+    }
+  })
+  .fail(function (err) {
+    throw err;
+  });
