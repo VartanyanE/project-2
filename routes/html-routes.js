1e0ec6b025d033890c67a8a8f7d10e32ab3490c1
Stephanie DeDios <stephtdedios@gmail.com>
2020-04-17 13:28:39 -0700

    Updated API's

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,14 +10,13 @@ var isNotAuthenticated2 = require("../config/middleware/isNotAuthenticated2");
 
 
 module.exports = function (app) {
-
   app.get("/", function (req, res) {
     // If the user already has an account send them to the members page
     // if (req.user) {
     //   res.redirect("/members");
     // }
     // res.sendFile(path.join(__dirname, "../public/index.html"));
-    res.render("index", { data: '' });
+    res.render("index", { data: "" });
   });
 
   app.get("/login", function (req, res) {
@@ -26,81 +25,67 @@ module.exports = function (app) {
       return res.redirect("/inventory");
     }
     // res.sendFile(path.join(__dirname, "../public/login.html"));
-    res.render("login", { data: '' });
+    res.render("login", { data: "" });
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function (req, res) {
     // res.sendFile(path.join(__dirname, "../public/members.html"));
-    res.render("members", { data: '' });
+    res.render("members", { data: "" });
   });
 
   app.get("/signup", function (req, res) {
     // res.sendFile(path.join(__dirname, "../public/members.html"));
-    res.render("signup", { data: '' });
+    res.render("signup", { data: "" });
   });
 
-  app.get("/covid-19", isNotAuthenticated, function (req, res, ) {
-    // const user = isAuthenticated;
-    return res.render("covid-19", {
-      sessionId: req.user.id
-
-    });
+  app.get("/covid-19", isNotAuthenticated, function (req, res) {
+    console.log("hello");
   });
 
   app.get("/index-user", isNotAuthenticated2, function (req, res) {
-
     res.render("index-user", {
-      sessionId: req.user.id
+      sessionId: req.user.id,
     });
   });
 
   app.get("/inventory", isAuthenticated, function (req, res) {
+    console.log(req.user.id);
 
+    db.Inventory.findAll({ where: { UserId: req.user.id } })
+      .then((data) => {
+        var jsonData = data.map((el) => el.toJSON());
 
-    console.log(req.user.id)
-
-    db.Inventory.findAll({ where: { UserId: req.user.id } }).then(data => {
-
-      var jsonData = data.map(el => el.toJSON())
-
-      // console.log(nodeData);
-      res.render("inventory", {
-        inventory: jsonData,
-        sessionId: req.user.id
+        // console.log(nodeData);
+        res.render("inventory", {
+          inventory: jsonData,
+          sessionId: req.user.id,
+        });
+      })
+      .catch((err) => {
+        // console.log(err)
+        res.status(404).end;
       });
-    }).catch(err => {
-      // console.log(err)
-      res.status(404).end
-
-    })
-
-
-
   });
 
   app.get("/shopping", isAuthenticated, function (req, res) {
+    console.log(req.user.id);
 
-    console.log(req.user.id)
+    db.ShoppingList.findAll({ where: { UserId: req.user.id } })
+      .then((data) => {
+        console.log("shopping_list:", data);
+        var jsonData = data.map((el) => el.toJSON());
 
-    db.ShoppingList.findAll({ where: { UserId: req.user.id } }).then(data => {
-
-      console.log("shopping_list:", data)
-      var jsonData = data.map(el => el.toJSON())
-
-      // console.log(nodeData);
-      return res.render("shopping", {
-        ShoppingList: jsonData,
-        sessionId: req.user.id
+        // console.log(nodeData);
+        return res.render("shopping", {
+          ShoppingList: jsonData,
+          sessionId: req.user.id,
+        });
+      })
+      .catch((err) => {
+        // console.log(err)
+        res.status(404).end;
       });
-    }).catch(err => {
-      // console.log(err)
-      res.status(404).end
-
-    })
-
   });
-
-
 };
